0ef2e1d8fad74c8e5cb37d1471c41acf1549884c
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-15 14:17:33 -0300

    feat: adiciona contexto para ficar disponível

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
+import StockContextProvider from './contexts/StockContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <RouterProvider router={router} />
+    <StockContextProvider>
+      <RouterProvider router={router} />
+    </StockContextProvider>
   </StrictMode>,
 )
