89c5b5f4e286e407ef473e1266b1c9135f8ec0c2
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-14 23:16:07 -0300

    feat: cria componente principal de adicionar novo item

---
+++ src/components/NewItem/index.tsx
@@ -0,0 +1,65 @@
+import "./index.css"
+import { useState } from "react"
+function NewItem() {
+  const [name, setName] = useState("")
+  const [qntd, setQntd] = useState<number>()
+  const [price, setPrice] = useState<number>()
+  const [category, setCategory] = useState<string[]>([])
+  const [description, setDescription] = useState("")
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>, ) {
+    e.preventDefault()
+    const data = {
+      name,
+      qntd,
+      price,
+      category,
+      description
+    }
+    console.log(data)
+  }
+  return (
+    <>
+      <form onSubmit={onSubmit} className="containerForm">
+        <div className="content">
+        <div className="inputGroup">
+        <div className="inputLabel">
+        <label id="name">Nome</label>
+        <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className="inputLabel">
+        <label id="qntd">Quantidade</label>
+        <input type="number" name="qntd" value={qntd} onChange={(e) => setQntd(+e.target.value)}/>
+
+        </div>
+        <div className="inputLabel">
+        <label id="price">Preço</label>
+        <input type="number" name="price" value={price} onChange={(e) => setPrice(+e.target.value)}/>
+
+        </div>
+        <div className="inputLabel">
+        <label id="categoryIpt">Categoria</label>
+        <input type="text" name="categoryIpt" list="category" 
+        placeholder="Selecione uma categoria..." value={category.join(", ")} 
+        onChange={(e) => setCategory(e.target.value.split(",").map(item => item.trim()))}/>
+        <datalist id="category">
+          <option value="Teste1" />
+          <option value="Teste2" />
+          <option value="Teste3" />
+          <option value="Teste4" />
+          <option value="Teste5" />
+        </datalist>
+        </div>
+        </div>
+        <div className="textArea">
+        <label id="description">Descrição</label>
+        <textarea name="description" rows={5} value={description} onChange={(e) => setDescription(e.target.value)} />
+        </div>
+        <button className="btnSave">Salvar</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default NewItem
