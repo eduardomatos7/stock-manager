758062077633910ad0b1cf51c718fbfe0d0ec710
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-16 15:54:38 -0300

    feat: renderiza valores dos items corretamente do dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,25 +1,32 @@
+import { useContext } from "react"
 import "../pages/Home/index.css"
 import Title from "./Title"
+import { StockContext } from "../contexts/StockContext"
 function Dashboard() {
+    const { items, itemsOut, itemsIndDate } = useContext(StockContext)
+    const totalInventory = items.reduce((acc, item) => acc + Number(item.quantidade), 0)
+    const uniqueItems = items.length
+    const recentItems = itemsIndDate.length
+    const lowStockItems = itemsOut.length
   return (
     <>
     <Title title="Dashboard"/>
                 <div className='cards'>
                     <div className='card'>
                         <p>Diversidade de itens</p>
-                        <p className='cardValueItem'>10</p>
+                        <p className='cardValueItem'>{uniqueItems}</p>
                     </div>
                     <div className='card'>
-                        <p>Diversidade de itens</p>
-                        <p className='cardValueItem'>10</p>
+                        <p>Inventário total</p>
+                        <p className='cardValueItem'>{totalInventory}</p>
                     </div>
                     <div className='card'>
-                        <p>Diversidade de itens</p>
-                        <p className='cardValueItem'>10</p>
+                        <p>Itens recentes</p>
+                        <p className='cardValueItem'>{recentItems}</p>
                     </div>
                     <div className='card'>
-                        <p>Diversidade de itens</p>
-                        <p className='cardValueItem'>10</p>
+                        <p>Itens acabando</p>
+                        <p className='cardValueItem'>{lowStockItems}</p>
                     </div>
                 </div>
 </>
