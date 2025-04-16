7c88789f5c8bc855837df0ff3583fc5e24477022
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-16 14:01:15 -0300

    feat(home): adiciona logica para renderizar todos os items na tabela e estiliza

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -1,61 +1,56 @@
+import { useContext } from "react"
+import { StockContext } from "../contexts/StockContext"
 
 function Tables() {
-  return (
-    <div className="sectionsToDetails">
-                    <div className="itemsRecent">
-                        <table className='tableRecent'>
-                            <thead>
-                            <tr >
-                                <th>Itens recentes</th>
-                                <th>Ações</th>
+    const { items } = useContext(StockContext)
+    return (
+        <div className="sectionsToDetails">
+            <div className="itemsRecent">
+                <table className='tableRecent'>
+                    <thead>
+                        <tr >
+                            <th>Itens recentes</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item) => {
+                            return <tr className="bgTr" key={item.id}>
+                                <td>{item.nome}</td>
+                                <td><button className='btnSee'>Ver</button></td>
                             </tr>
-                            </thead>
-                            <tbody>
+                        })
 
-                            <tr>
-                                <td>7 Wonders</td>
-                                <td><button className='btnSee'>Ver</button></td>
-                            </tr>
-                            <tr>
-                                <td>7 Wonders</td>
-                                <td><button className='btnSee'>Ver</button></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="itemsOut">
-                        <table>
-                            <thead>
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <div className="itemsOut">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Itens acabando</th>
+                            <th>Qntd.</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item ) => {
+                            return <tr className="bgTr">
+                            <td>{item.nome}</td>
+                            <td className="algnAbs">{item.quantidade}</td>
+                            <td><button className='btnSee'>Ver</button></td>
+                        </tr>
+                        })
+                       
+                        }
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-                            <tr>
-                                <th>Itens acabando</th>
-                                <th>Qntd.</th>
-                                <th>Ações</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-
-                            <tr>
-                                <td>7 Wonders</td>
-                                <td className="algnAbs">8</td>
-                                <td><button className='btnSee'>Ver</button></td>
-                            </tr>
-                            <tr>
-                                <td>7 Wonders</td>
-                                <td className='algnAbs'>8</td>
-                                <td><button className='btnSee'>Ver</button></td>
-                            </tr>
-                            <tr>
-                                <td>7 Wonders</td>
-                                <td className='algnAbs'>8</td>
-                                <td><button className='btnSee'>Ver</button></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-  )
+    )
 }
 
 export default Tables
